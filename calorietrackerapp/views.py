3b1be0a6f2b19c9b0f11b72bbb28139c9dc72251
sirishacyd <sirishacyd@gmail.com>
2022-11-08 00:26:27 -0800

    Change Code for aws cognito

--- calorietrackerapp/views.py
+++ calorietrackerapp/views.py
@@ -6,6 +6,11 @@ from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from . import decode_jwt
+from django.shortcuts import render
+from decouple import config
+import base64
+import requests
 
 from .models import User, Food_Obj, Food_Cat, Food_log_mdl, Image, UserWeight
 from .forms import FoodForm, ImageForm
@@ -13,6 +18,34 @@ from .forms import FoodForm, ImageForm
 
 def index(request):
     return foodlist(request)
+
+def index1(request):
+    # print("I'm in!")
+    try:
+        code = request.GET.get('code')
+        # print(code)
+        userData = getTokens(code)
+        print(userData)
+        context = {'name': userData['name'], 
+                    'email': userData['email'],
+                    'status': 1,}
+        print(context)
+        response = render(request, 'core/index.html' , context)
+        response.set_cookie('sessiontoken', userData['id_token'], max_age=60*60*24, httponly=True)
+
+        return response
+    except Exception as e:
+        token = getSession(request)
+        if token is not None:
+            userData = decode_jwt.lambda_handler({'token':token}, None)
+            context = {'name': userData['name'], 
+                    'email': userData['email'],
+                    'status': 1,}
+            print(context)
+            render(request, 'core/index.html' , context)
+
+        print("No code")
+        return render(request, 'core/index.html', {'status': 0})
 
 
 def login_v(request):
@@ -40,6 +73,10 @@ def logout_v(request):
     logout(request)
     return HttpResponseRedirect(reverse('login'))
 
+def signout(request):  
+    response =  render(request, 'core/index.html', {'status': 0}) 
+    response.delete_cookie('sessiontoken')
+    return response
 
 
 def register(request):
@@ -241,11 +278,45 @@ def Food_log_mdldelete(request, food_id):
     return render(request, 'food_log_delete.html', {
         'categories': Food_Cat.objects.all()
     })
+def getTokens(code):
+    TOKEN_ENDPOINT = config('TOKEN_ENDPOINT')
+    REDIRECT_URI = config('REDIRECT_URI')
+    CLIENT_ID = config('CLIENT_ID') 
+    CLIENT_SECRET = config('CLIENT_SECRET')
+
+    encodeData = base64.b64encode(bytes(f"{CLIENT_ID}:{CLIENT_SECRET}", "ISO_8859-1")).decode("ascii")
+    # print(encodeData)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': f'Basic {encodeData}'
+    }
+
+    body = {
+        'grant_type': 'authorization_code',
+        'client_id': CLIENT_ID,
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+    }
+    # print(body)
+    response = requests.post(TOKEN_ENDPOINT, data=body, headers=headers)
+    # print("response", response)
+    id_token = response.json()['id_token']
+    # print("id_token", id_token)
+    userData = decode_jwt.lambda_handler({'token':id_token}, None)
+    # print("userData", userData)
+    if not userData:
+        return False 
+    user = {
+        'id_token': id_token,
+        'name': userData['cognito:username'],
+        'email': userData['email']
+    }
+    # print(user)
+    return user
 
 
-    
-
-        
-
-
-
+def getSession(request):
+    try:
+        response = request.COOKIES["sessiontoken"]
+    except:
+        return None
