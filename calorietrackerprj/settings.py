3f20eae5b8044300944a0ba477c50b90ab267c5b
Blessy Dickson <dblessy@gmail.com>
2022-11-12 00:11:28 -0800

    add elasticache middleware

--- calorietrackerprj/settings.py
+++ calorietrackerprj/settings.py
@@ -31,13 +31,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'calorietrackerprj.urls'
